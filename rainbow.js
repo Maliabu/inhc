bcd23bfa012bc1d4d58ba6a6200918a780e9afb0
Maliabu <giramiapatricia61@gmail.com>
2021-10-07 08:29:19 +0300

    no python

---
+++ rainbow.js
@@ -0,0 +1,86 @@
+export default 
+{
+    "color": {
+        "red": {
+            "item": {
+                "name": "tomato",
+                "nutrients": "vitamins"
+            },
+            "chilli": {
+                "nutrients": [ "vitamins", "potassium" ]
+            },
+            "red-pepper": {
+                "nutrients": []
+            },
+            "red-apples": {
+                "nutrients": []
+            }
+        },
+        "orange": {
+            "carrots": {
+                "nutrients": "vitamins"
+            },
+            "oranges": {
+                "nutrients": "vitamins"
+            }
+        },
+        "yellow": {
+            "banana": {
+                "nutrients": [ "vitamins", "potassium", "magnesium"]
+            },
+            "corn": {
+                "nutrients": ["carbohydrates"]
+            },
+            "yellow-pepper": {
+                "nutrients": []
+            },
+            "pineapples": {
+                "nutrients": []
+            }
+        },
+        "green": {
+            "greens": {
+                "nutrients":"vitamins"
+            },
+            "green-pepper": {
+                "nutrients": []
+            },
+            "green-beans": {
+                "nutrients": ["proteins"]
+            },
+            "green-apples": {
+                "nutrients": []
+            },
+            "lemon": {
+                "nutrients": []
+            }
+        },
+        "blue": {
+            "blueberries": {
+                "nutrients": [ "vitamins", "copper"]
+            }
+        },
+        "indigo": {
+            "beans": {
+                "nutrients": [ "magnesium", "magnesium", "proteins", "iron"]
+            },
+            "groundnuts": {
+                "nutrients": ["proteins"]
+            },
+            "meats": {
+                "nutrients": ["proteins"]
+            }
+        },
+        "violet": {
+            "eggplant": {
+                "nutrients": [ "potassium", "vitamins", "manganese", "proteins"]
+            },
+            "onions": {
+                "nutrients": [""]
+            },
+            "grapes": {
+                "nutrients": []
+            }
+        }
+    }
+}
