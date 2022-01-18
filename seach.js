807e006cef2178cbd0cc742693e0377355e90a10
Giramia Patricia <60360658+Maliabu@users.noreply.github.com>
2022-01-18 10:33:40 +0300

    Add files via upload

--- seach.js
+++ seach.js
@@ -6,18 +6,18 @@ function search() {
 
     // console.log(((list[0].innerHTML).toLowerCase()).includes("pyt", 0))
     list.forEach((lang) => {
-        if ((lang.innerHTML.toLowerCase()).includes(input, 0) === false){
+        if ((lang.innerHTML.toLowerCase()).includes(input, 0) === false) {
             lang.style.display = "none";
         } else {
             lang.style.display = "list-item";
         }
 
-        if (lang.style.display === "none"){
-           displayCounter++;
+        if (lang.style.display === "none") {
+            displayCounter++;
         }
     });
 
-    if (displayCounter === list.length){
+    if (displayCounter === list.length) {
         list.innerHTML = "No items to display";
         noItemsText.style.display = "initial"
     } else {
@@ -27,17 +27,30 @@ function search() {
 /////////////////////////////////////////////////////////
 var clicks = 0;
 var clicking = 0;
-function onClicked(){
-    clicking+=1
+
+function onClicked() {
+    clicking += 1
     document.getElementById("clicked").innerHTML = clicking;
 }
+
 function onClick() {
-  clicks += 1;
-  
-  document.getElementById("clicks").innerHTML = clicks;
-  // Store
-  localStorage.setItem("clicks", clicks);
-  // Retrieve
-  document.getElementById("clicks").innerHTML = localStorage.getItem("clicks");
+    clicks += 1;
+
+    document.getElementById("clicks").innerHTML = clicks;
+    // Store
+    localStorage.setItem("clicks", clicks);
+    // Retrieve
+    document.getElementById("clicks").innerHTML = localStorage.getItem("clicks");
 }
 /////////////////////////////////////////////////////////
+/////////// date footer /////////////////////////////////
+var prev = Date();
+var dater = document.getElementById("date").innerHTML = prev;
+
+///////////////////// discussion ///////////////////////
+function discus() {
+    var dis = document.getElementById("mat");
+    if (dis) {
+        document.getElementById("maternals").style.input = 'checked';
+    }
+}
